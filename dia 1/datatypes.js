9cdbc96291c7be710157a4c7d0ae93cca640c3f6
enriquejgilq <35854635+enriquejgilq@users.noreply.github.com>
2022-06-12 20:36:30 -0500

    dia 1

---
+++ dia 1/datatypes.js
@@ -0,0 +1,28 @@
+//Bienvenido a 30DaysOfJavaScript 
+/*en este  archivo 
+se pone en practica lo aprendido en el primer dia. :D */
+//declarar 4 variables sin asignar valor
+let one 
+let two
+let three
+let four
+
+//declarar 4 variables con valor 
+let oneValue = 1
+let twoValue = 2
+let threeValue = 3
+let fourValue = 4
+
+//declarar 4 variables para almacenar nombre apellido estado civil pais y edad en varias lineas
+let nameExample = 'Enrique'
+let lastNameExample = 'Gil'
+let stateExample = 'Soltero'
+let countryExample = 'Venezuela'
+
+// declarar 4 variables para almacenar nombre apellido estado civil pais y edad en una sola linea
+let nameOneLine = 'Enrique', lastNameOneLine = 'Gil', stateOneLine = 'Soltero', countryOneLine = 'Venezuela'
+
+//declarar dos variables myAge y yourAge y asignarle valores
+let myAge = 30
+let yourAge = 40
+console.log('yo tengo ' + myAge + ' años y tu tienes ' + yourAge + ' años')
---
+++ dia 1/introduccion.js
@@ -0,0 +1,23 @@
+console.log('introduccion.js');
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+// Declaring different variables of different data types
+let firstNameE = 'Asabeneh' // first name of a person
+let lastNameE = 'Yetayeh' // last name of a person
+let country = 'Finland' // country
+let city = 'Helsinki' // capital city
+let ageE = 100 // age in years
+let isMarried = true
+
+console.log(firstNameE, lastNameE, country, city, ageE, isMarried)
+
+// Declaring variables with number values
+let age2 = 100 // age in years
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in °C
+const PI = 3.14 // geometrical constant
+console.log(gravity, boilingPoint, PI)
+
